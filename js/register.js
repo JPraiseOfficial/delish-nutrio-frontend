6cf647246a536a297f374432f44b970e29358a0d
Jimmy Praise <93206346+JPraiseOfficial@users.noreply.github.com>
2025-01-13 15:54:22 +0100

    corrected error in redirect URL

--- js/register.js
+++ js/register.js
@@ -27,7 +27,7 @@ form.addEventListener('submit', function (event) {
         })
         .then(data => {  
             console.log('Success:', data); 
-            window.location.href = '../verification.html'; 
+            window.location.href = './verification.html'; 
         })  
         .catch(error => {  
             console.error('Error:', error);  
