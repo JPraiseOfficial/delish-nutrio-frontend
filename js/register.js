b969cc0f0f4761dee5f3da18189cc0d5eef52f50
JPraiseOfficial <jimmypraiseofficial@gmail.com>
2025-01-14 16:32:10 +0100

    used ES modules to write register.js and set global variables

--- js/register.js
+++ js/register.js
@@ -1,35 +1,41 @@
+import { backendServer } from "./global_variables.js";
 const form = document.getElementById('register-form');
+const formErrors = document.querySelectorAll('.form-error')
+console.log(formErrors)
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-
     const formData = new FormData(form);
-
-    const data = Object.fromEntries(formData);
+    const formDataObject = Object.fromEntries(formData);
 
     const options = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
-          },
-        body: JSON.stringify(data),
+        },
+        body: JSON.stringify(formDataObject),
     }
 
-    fetch('https://delish-nutrio.onrender.com/api/register', options)
-        .then(response => {
-            if (!response.ok) {
-                return response.text().then(text => {
-                    console.log(text);
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                });
+    const register = async () => {
+        try {
+            const response = await fetch(`${backendServer}/api/register`, options, {
+                method: 'POST',
+            });
+
+            if (response.ok) {
+                console.log('Success:', responseData);
+                window.location.href = './verification.html';
+            } else {
+                const responseData = await response.json();
+                console.log(responseData)
+
             }
-            return response.json();
-        })
-        .then(data => {  
-            console.log('Success:', data); 
-            window.location.href = './verification.html'; 
-        })  
-        .catch(error => {  
-            console.error('Error:', error);  
-        }); 
+
+        } catch (error) {
+            console.log('An error occurred')
+        }
+
+    }
+    register()
+
 })
